691752fdfa804d3eab93e2f84b20268a07bece6d
Dududenis <dudusjp3@hotmail.com>
2021-12-22 14:34:01 -0300

    Adiciona questao 03 e 04

---
+++ questao_03.py
@@ -0,0 +1,6 @@
+#)3 Escreva uma função que receba o lado (l) de um quadrado e retorne sua área (A = lado^2).
+
+def retorna_quadrado(lado):
+   return lado ** 2
+print(retorna_quadrado(4))
+    
---
+++ questao_04.py
@@ -0,0 +1,7 @@
+#4) Escreva uma função que receba a base e a altura de um triângulo e retorne sua área (A = (base x altura)/2)
+
+def retorna_area(base,altura):
+    return (base * altura) / 2
+
+print(retorna_area(6,9))
+print(retorna_area(5,8))
